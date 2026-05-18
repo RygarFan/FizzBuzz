a881a85a331379d2ad8cc3866a829438df22fdcf
RygarFan <oscale05@gmail.com>
2026-05-17 19:30:29 -0600

    C# added

---
+++ programs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+
+            while (true)
+            {
+                if (n % 3 == 0)
+                {
+                    if (n % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                }else if (n % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(n);
+                }
+
+                n++;
+            }
+        }
+    }
+}
